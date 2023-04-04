66261507cd8b024714278ae50ed8a52a5655f03b
m0slah <muhammedslahqadir@gmail.com>
2023-04-04 17:47:54 +0300

    add all components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 
+import Product from "../../Components/Product";
+import HeroBanner from "Components/HeroBanner";
+import Footer from "Components/Footer";
+
 function Home() {
   return (
     <React.Fragment>
-      Hero Banner Comp
+      <HeroBanner />
+
       <div className="products-heading">
         <h2>Best Selling Product</h2>
         <p>Speaking of many variations</p>
       </div>
+      
       <div className="products-container">
         {["product1", "product2"].map((product) => product)}
       </div>
-      footer
+
+      <Footer />
     </React.Fragment>
   );
 }
