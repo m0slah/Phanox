657b063666f46978ad808ba01ff78989a5adb1e2
m0slah <muhammedslahqadir@gmail.com>
2023-04-04 17:59:19 +0300

    add heroBanner Structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 
-import Product from "../../Components/Product";
 import HeroBanner from "Components/HeroBanner";
-import Footer from "Components/Footer";
+import FooterBanner from "Components/FooterBanner";
 
 function Home() {
   return (
@@ -13,12 +12,12 @@ function Home() {
         <h2>Best Selling Product</h2>
         <p>Speaking of many variations</p>
       </div>
-      
+
       <div className="products-container">
         {["product1", "product2"].map((product) => product)}
       </div>
 
-      <Footer />
+      <FooterBanner />
     </React.Fragment>
   );
 }
