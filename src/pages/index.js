fa3c90b1d9cbc849e10724e687acec5996f744c7
m0slah <muhammedslahqadir@gmail.com>
2023-04-30 15:14:31 +0300

    add prodcut

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,18 @@ import product from 'sanity-ecommerce/schemas/product';
 
 const Home = ({ products, bannerData }) => (
   <div>
-    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
-    <div className="products-heading">
-      <h2>Best Seller Products</h2>
-      <p>speaker There are many variations passages</p>
-    </div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+    
+      <div className="products-heading">
+        <h2>Best Seller Products</h2>
+        <p>speaker There are many variations passages</p>
+      </div>
 
     <div className="products-container">
-      {products?.map((product) => product.name)}
+      {products?.map((product) => <Product key={product._id} product={ product  } />)}
     </div>
 
-    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+    <FooterBanner  footerBanner={bannerData && bannerData[0]} />
   </div>
 );
 
