db9c99c74aa97b5f0d87be776aad9ed9100c5c7f
m0slah <muhammedslahqadir@gmail.com>
2023-04-30 16:05:41 +0300

    add footerBanner

--- Components/FooterBanner.jsx
+++ Components/FooterBanner.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
-function FooterBanner() {
+import { urlFor } from "Lib/client";
+import product from "sanity-ecommerce/schemas/product";
+
+const FooterBanner = ({
+  footerBanner: {
+    image,
+    discount,
+    largeText1,
+    largeText2,
+    saleTime,
+    smallText,
+    midText,
+    desc,
+    buttonText,
+  },
+}) => {
   return (
-    <div>FooterBanner</div>
-  )
-}
+    <div className="footer-banner-container">
+      <div className="banner-desc">
+        <div className="left">
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
+        </div>
+        <div className="right">
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
+          </Link>
+          <img src={urlFor(image)} className="footer-banner-image" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default FooterBanner
+export default FooterBanner;
