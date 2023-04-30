fbbe8fd17de4e6c36d88a573ce3855cc8f58e531
m0slah <muhammedslahqadir@gmail.com>
2023-04-30 21:10:14 +0300

    add footer

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
+import { AiFillInstagram } from "react-icons/ai";
+import { BsTwitter } from "react-icons/bs";
+
+
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p>2023 JSM Headphones All right reserverd</p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <BsTwitter/>
+      </p>
+    </div>
   )
 }
 
