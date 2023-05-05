76e4fe02e27e9f5c8bcdfb177dc82c325458d8fd
m0slah <muhammedslahqadir@gmail.com>
2023-05-05 23:28:28 +0300

    organize HeroBanner.jsx code

--- Components/HeroBanner.jsx
+++ Components/HeroBanner.jsx
@@ -3,25 +3,29 @@ import Link from "next/link";
 
 import { urlFor } from "Lib/client";
 
-function HeroBanner({heroBanner}) {
+function HeroBanner({ heroBanner }) {
   return (
     <div className="hero-banner-container">
       <div>
         <p>{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
-        <h1>{ heroBanner.largeText1}</h1>
-        <img src={urlFor(heroBanner.image)} alt="Headphones" className="hero-banner-image" />
+        <h1>{heroBanner.largeText1}</h1>
+        <img
+          src={urlFor(heroBanner.image)}
+          alt="Headphones"
+          className="hero-banner-image"
+        />
+      </div>
+
+      <div>
+        <Link href={`/product/${heroBanner.product}`}>
+          <button type="button">{heroBanner.buttonText}</button>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
           </div>
-          
-          <div>
-              <Link href={`/product/${heroBanner.product}`}>
-          <button type="button">{ heroBanner.buttonText}</button>
-                  <div className="desc">
-                      <h5>Description</h5>
-                      <p>{heroBanner.desc}</p>
-                  </div>
-              </Link>
-          </div>
+        </Link>
+      </div>
     </div>
   );
 }
