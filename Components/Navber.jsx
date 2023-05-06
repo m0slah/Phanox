c374268303c8572ec53860e15a2ec22d25a46592
m0slah <muhammedslahqadir@gmail.com>
2023-05-06 21:44:31 +0300

    add Cart in Navbar

--- Components/Navber.jsx
+++ Components/Navber.jsx
@@ -4,19 +4,30 @@ import Link from "next/link";
 
 import { AiOutlineShoppingCart } from "react-icons/ai";
 
-const Navber=()=> {
+import Cart from "./Cart";
+import { useStateContext } from "context/StateContext";
+
+const Navber = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
   return (
     <div className="navbar-container">
       <p className="logo">
         <Link href="/">JSM HeadPhones</Link>
       </p>
 
-      <button type="button" className="cart-icon" onClick="">
+      <button
+        type="button"
+        className="cart-icon"
+        onClick={() => setShowCart(true)}
+      >
         <AiOutlineShoppingCart />
-        <span className="cart-item-qty">1</span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
+
+      {showCart && <Cart />}
     </div>
   );
-}
+};
 
 export default Navber;
