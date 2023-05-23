6951ef38e423efd3e1c318c1a874b7ae7f0179f1
m0slah <muhammedslahqadir@gmail.com>
2023-05-23 17:26:44 +0300

    redirect if canceled

--- src/pages/api/stripe.js
+++ src/pages/api/stripe.js
@@ -44,7 +44,7 @@ export default async function handler(req, res) {
           };
         }),
         success_url: `${req.headers.origin}/success`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
+        cancel_url: `${req.headers.origin}/canceled`,
       };
 
       const session = await stripe.checkout.sessions.create(params);
